3a134da901f84f50e75c33b8f72035f06cb8d766
siva <siva@google.com>
2023-10-29 07:34:11 +0530

    Added code till k8s

---
+++ src/com/i27academy/k8s/K8s.groovy
@@ -0,0 +1,28 @@
+package com.i27academy.k8s
+
+class K8s {
+    def jenkins 
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    def auth_login(gke_cluster_name, gke_zone, gke_project){
+        jenkins.sh"""#!/bin/bash
+        echo "Entering Auth Method for k8s Login"
+        gcloud config set account jenkins-gke-svc-account@practical-brace-402514.iam.gserviceaccount.com
+        #gcloud auth activate-service-account jenkins-gke-svc-account@practical-brace-402514.iam.gserviceaccount.com --key-file=key.json
+        gcloud compute instances list
+        echo "***************** Showing Number of Worker node in cluster *****************"
+        gcloud container clusters get-credentials $gke_cluster_name --zone $gke_zone --project $gke_project
+        kubectl get nodes 
+        """
+    }
+    def k8sdeploy(fileName, docker_image) {
+        jenkins.sh """#!/bin/bash
+        echo "Executing K8S Deploy Method"
+        echo "Final image tag is $docker_image"
+        sed -i "s|DIT|$docker_image|g" ./.cicd/$fileName
+        kubectl apply -f ./.cicd/$fileName
+        """
+    }
+}
