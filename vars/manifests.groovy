ecae8975522934ce2e3a15f7430e5334f3fd1897
siva <siva@google.com>
2023-11-05 09:27:11 +0530

    Adde qioptes'

--- vars/manifests.groovy
+++ vars/manifests.groovy
@@ -19,7 +19,7 @@ def call(Map pipelineParams) {
             // enter the namespace name 
             string (name: 'NAMESPACE_NAME', description: "Enter the name of the kubernetes namespace to be created")
             string (name: 'netpolName', description: "Enter the name of the netpol")
-            booleanParam(name: AddNetworkPolicy,
+            booleanParam(name: 'AddNetworkPolicy',
                 defaultValue: 'false',
                 description: 'Enable this checkbox, if you need a default netpol in your namespace')
         }
